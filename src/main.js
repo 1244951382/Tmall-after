e82acbb8ef45296b2daa7850aa97d5231371d88c
Charon.Wang <1244951382@qq.com>
2023-02-06 21:41:19 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.config.productionTip = false
+//引入全局样式
+import './assets/gloable.css'
+//引入request.js
+import request from '@/utils/request.js'
+import store from './store/store';
+
+Vue.config.productionTip = false;
+//引入request.js
+Vue.prototype.request = request;
+
+
+//设置大小，适配屏幕
+Vue.use(ElementUI, {size: "mini"});
+
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,46 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import store from "@/store/store";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    component: () => import('../views/Manage.vue'),
+    redirect: "/home",
+    children: [
+      //  首页
+      {path: 'home', name: '首页', component: () => import('../views/Home.vue')},
+      //  用户管理
+      {path: 'user', name: '用户管理', component: () => import('../views/User.vue')},
+      //  角色管理
+      {path: 'role', name: '角色管理', component: () => import('../views/Role.vue')},
+      //  用户信息
+      {path: 'person', name: '个人信息', component: () => import('../views/Person.vue')},
+      //  菜单信息
+      {path: 'menu', name: '菜单信息', component: () => import('../views/Menu.vue')},
+      //  文件管理
+      {path: 'file', name: '文件管理', component: () => import('../views/File.vue')}
+    ]
   },
+  //  关于页面
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    name: 'About',
+    component: () => import('../views/About.vue')
+  },
+  //用户登录界面
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  //用户注册界面
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('../views/Register.vue')
   }
 ]
 
@@ -24,6 +48,13 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+// 路由守卫
+router.beforeEach((to, from, next) => {
+  localStorage.setItem("currentPathName", to.name)  // 设置当前的路由名称，为了在Header组件中去使用
+  store.commit("setPath")  // 触发store的数据更新
+  next()  // 放行路由
 })
 
 export default router
